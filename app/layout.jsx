29c0048b5687cfcde5120430e564009af70ea66e
jawadchy2150 <jawadchowdhury260@gmail.com>
2024-11-23 22:04:12 +0600

    Initial commit

---
+++ app/layout.jsx
@@ -0,0 +1,21 @@
+import '@/assets/style/globals.css'
+
+export const metadata = {
+    title: 'PropertyPulse | Find the Perfect Rental',
+    description: 'Find your dream rental property',
+    keywords: 'rental, find rentals, find properties'
+}
+
+const MainLayout = ( { children }) => {
+  return (
+    <html lang="en">
+        <body>
+            <div>
+                {children}
+            </div>
+        </body>
+    </html>
+  )
+}
+
+export default MainLayout; 
